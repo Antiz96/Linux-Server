c4fa66a6afb903cf0805520561b5b9ec3dce86cc
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2023-12-13 21:25:51 +0100

    Add Jenkins groovy pipeline to update the all infra (#183)

---
+++ Dotfiles/Services/Jenkins_Pipeline_Update_Infra.groovy
@@ -0,0 +1,181 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Update Docker Containers - VMs - Prod') {
+            steps {
+                script {
+                    def result = build(
+                        job: 'Update_Docker_Containers',
+                        parameters: [
+                            string(name: 'SERVER_TYPE', value: 'VMs'),
+                            string(name: 'ENV', value: 'prod')
+                        ],
+                        propagate: true,
+                        wait: true
+                    )
+                    if (result == 'FAILURE') {
+                        error("Update Docker Containers - VMs - Prod failed. Aborting pipeline.")
+                    }
+                }
+            }
+        }
+
+        stage('Update Docker Containers - Proxmox - Prod') {
+            steps {
+                script {
+                    def result = build(
+                        job: 'Update_Docker_Containers',
+                        parameters: [
+                            string(name: 'SERVER_TYPE', value: 'Proxmox'),
+                            string(name: 'ENV', value: 'prod')
+                        ],
+                        propagate: true,
+                        wait: true
+                    )
+                    if (result == 'FAILURE') {
+                        error("Update Docker Containers - Proxmox - Prod failed. Aborting pipeline.")
+                    }
+                }
+            }
+        }
+
+        stage('Update Servers - VMs - Dev - All') {
+            steps {
+                script {
+                    def result = build(
+                        job: 'Update_Servers',
+                        parameters: [
+                            string(name: 'SERVER_TYPE', value: 'VMs'),
+                            string(name: 'ENV', value: 'dev')
+                            string(name: 'HOSTS', value: 'all')
+                        ],
+                        propagate: true,
+                        wait: true
+                    )
+                    if (result == 'FAILURE') {
+                        error("Update Servers - VMs - Dev - All failed. Aborting pipeline.")
+                    }
+                }
+            }
+        }
+
+        stage('Update Servers - VMs - Prod - Pmx01') {
+            steps {
+                script {
+                    def result = build(
+                        job: 'Update_Servers',
+                        parameters: [
+                            string(name: 'SERVER_TYPE', value: 'VMs'),
+                            string(name: 'ENV', value: 'prod')
+                            string(name: 'HOSTS', value: 'pmx01')
+                        ],
+                        propagate: true,
+                        wait: true
+                    )
+                    if (result == 'FAILURE') {
+                        error("Update Servers - VMs - Prod - Pmx01 failed. Aborting pipeline.")
+                    }
+                }
+            }
+        }
+
+        stage('Update Servers - VMs - Prod - Pmx02') {
+            steps {
+                script {
+                    def result = build(
+                        job: 'Update_Servers',
+                        parameters: [
+                            string(name: 'SERVER_TYPE', value: 'VMs'),
+                            string(name: 'ENV', value: 'prod')
+                            string(name: 'HOSTS', value: 'pmx02')
+                        ],
+                        propagate: true,
+                        wait: true
+                    )
+                    if (result == 'FAILURE') {
+                        error("Update Servers - VMs - Prod - Pmx02 failed. Aborting pipeline.")
+                    }
+                }
+            }
+        }
+
+        stage('Update Servers - VPS - Prod - All') {
+            steps {
+                script {
+                    def result = build(
+                        job: 'Update_Servers',
+                        parameters: [
+                            string(name: 'SERVER_TYPE', value: 'VPS'),
+                            string(name: 'ENV', value: 'prod')
+                            string(name: 'HOSTS', value: 'all')
+                        ],
+                        propagate: true,
+                        wait: true
+                    )
+                    if (result == 'FAILURE') {
+                        error("Update Servers - VPS - Prod - All failed. Aborting pipeline.")
+                    }
+                }
+            }
+        }
+
+        stage('Update Servers - Rasp - Prod - All') {
+            steps {
+                script {
+                    def result = build(
+                        job: 'Update_Servers',
+                        parameters: [
+                            string(name: 'SERVER_TYPE', value: 'Rasp'),
+                            string(name: 'ENV', value: 'prod')
+                            string(name: 'HOSTS', value: 'all')
+                        ],
+                        propagate: true,
+                        wait: true
+                    )
+                    if (result == 'FAILURE') {
+                        error("Update Servers - Rasp - Prod - All failed. Aborting pipeline.")
+                    }
+                }
+            }
+        }
+
+        stage('Update Servers - Proxmox - Prod - Pmx02') {
+            steps {
+                script {
+                    def result = build(
+                        job: 'Update_Servers',
+                        parameters: [
+                            string(name: 'SERVER_TYPE', value: 'Proxmox'),
+                            string(name: 'ENV', value: 'prod')
+                            string(name: 'HOSTS', value: 'pmx02')
+                        ],
+                        propagate: true,
+                        wait: true
+                    )
+                    if (result == 'FAILURE') {
+                        error("Update Servers - Proxmox - Prod - Pmx02 failed. Aborting pipeline.")
+                    }
+                }
+
+        stage('Update Servers - Proxmox - Prod - Pmx01') {
+            steps {
+                script {
+                    def result = build(
+                        job: 'Update_Servers',
+                        parameters: [
+                            string(name: 'SERVER_TYPE', value: 'Proxmox'),
+                            string(name: 'ENV', value: 'prod')
+                            string(name: 'HOSTS', value: 'pmx01')
+                        ],
+                        propagate: true,
+                        wait: true
+                    )
+                    if (result == 'FAILURE') {
+                        error("Update Servers - Proxmox - Prod - Pmx01 failed. Aborting pipeline.")
+                    }
+                }
+            }
+        }
+    }
+}
